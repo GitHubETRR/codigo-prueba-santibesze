cc4969127e8349895199e401e11c23897b758e8a
santibesze <sbesze@etrr.edu.ar>
2024-10-18 14:22:50 -0300

    tabla de multiplicar

---
+++ Codigos/Codigo.58.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void tablaDeMultiplicar(int num){
+    for (int i = 0; i <= 10; i++)
+    {
+        int res=i*num;
+    }
+}
+int main(){
+        int num;
+        printf("Introduzca un numero entero:\n");
+        scanf("%d",&num)
+        tablaDeMultiplicar(num);
+        printf("%d*%d=%d"i,num,res);
+    }
